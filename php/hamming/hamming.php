89cc8c0440b3b298c9cf1614ece389175d3fe160
Troy Nichols <troygnichols@gmail.com>
2019-08-02 03:50:54 -0400

    Improve php formatting

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,20 +1,19 @@
 <?php
 
-function distance(string $strandA, string $strandB): int {
-  if (strlen($strandA) != strlen($strandB))
-  {
-    throw new InvalidArgumentException('DNA strands must be of equal length.');
-  }
-
-  $distance = 0;
-
-  for ($i = 0; $i < strlen($strandA); $i++)
-  {
-    if ($strandA[$i] != $strandB[$i])
-    {
-      $distance++;
+function distance(string $strandA, string $strandB): int
+{
+    $len = strlen($strandA);
+    if ($len != strlen($strandB)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
-  }
 
-  return $distance;
+    $distance = 0;
+
+    for ($i = 0; $i < $len; $i++) {
+        if ($strandA[$i] != $strandB[$i]) {
+            $distance++;
+        }
+    }
+
+    return $distance;
 }
